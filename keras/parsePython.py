e1ff3288f10e12984a50c4d9c3f99bf258e49ac5
Jiansi Wang <zzbslayer@sjtu.edu.cn>
2018-09-26 16:05:59 +0800

    parse python complete

--- keras/parsePython.py
+++ keras/parsePython.py
@@ -6,14 +6,32 @@ class CodeVisitor(ast.NodeVisitor):
     apiSequence = []
     def __init__(self):
         self.methodName = []
-        self.nodeType = ''
+        self.nodeType = ""
         self.apiSequence = []
+        self.classInstance = {}
 
-    def str_node(self, node):
+        self.classLevel = -1
+
+        # e.g. temp = A()
+        #
+        # self.tempClass = A
+        self.tempClass = "" 
+
+        # e.g. class A:
+        #          def a:
+        #              ...
+        #
+        # self.generalClass = A
+        self.generalClass = ""
+
+    def str_node(self, node, level):
+        if (level <= self.classLevel):
+            self.classLevel = -1
+            self.generalClass = ""
         if isinstance(node, ast.AST):
             className = node.__class__.__name__
 
-            fields = [(name, self.str_node(val)) for name, val in ast.iter_fields(node) if name not in ('left', 'right')]
+            fields = [(name, self.str_node(val, level)) for name, val in ast.iter_fields(node) if name not in ('left', 'right')]
             rv = '%s(%s' % (className, ', '.join('%s=%s' % field for field in fields))
 
             if (self.nodeType == "FunctionDef"):
@@ -23,29 +41,77 @@ class CodeVisitor(ast.NodeVisitor):
                     self.methodName.append(fname)
             elif (self.nodeType == "Statement"):
                 if (className == "Expr"):
-                    print("Expr:")
+                    #print("##########")
                     message = fields[0][1]
-                    print(fields)
+                    #print(message)
+                    char = message[10]
+                    #print(char)
+                    if (char == 'N'):
+                        message = message[19:]
+                        api = ""
+                        for i in message:
+                            if (i == "'"):
+                                break
+                            api += i
+                        self.apiSequence.append(api)
+                        #print(api)
+                    if (char == 'A'):
+                        message = message[35:]
+                        #print(message)
+                        instanceName = ""
+                        for i in message:
+                            if (i =="'"):
+                                break
+                            instanceName += i
+                        message = message[len(instanceName)+22:]
+                        #print(message)
+
+                        className = ""
+                        if (instanceName == "self"):
+                            className = self.generalClass
+                        else:
+                            className = self.classInstance[instanceName]
+
+                        methodName = ""
+                        for i in message:
+                            if (i =="'"):
+                                break
+                            methodName += i
+                        self.apiSequence.append(className + "." + methodName)
+                        
                     #print(message[10:])
+                    #self.apiSequence.append(api)
 
                 elif (className == "Assign"):
                     #print("Assign:")
                     callInfo = fields[1][1]
-                    #print(fields[1])
+                    #print(fields)
                     callInfo = callInfo[callInfo.find("id"):]
-                    api = ""
+                    className = ""
                     for char in callInfo[4:]:
                         if (char !="'"):
-                            api += char
+                            className += char
                         else:
                             break
-                    self.apiSequence.append(api)
+                    self.apiSequence.append(className)
+                    self.tempClass = className
+                elif (className == "Name" and self.tempClass != ""): 
+                    #print(fields)
+                    tempClass = self.tempClass
+                    tempInstance = fields[0][1][1:-1]
+                    self.tempClass = ""
+                    self.classInstance[tempInstance] = tempClass
+                    #print(self.classInstance)
+                elif (className == "ClassDef"):
+                    self.generalClass = fields[0][1][1:-1]
+                    self.classLevel = level
+                    
             return rv + ')'
         else:
             return repr(node)
 
     def ast_visit(self, node, level=0):
-        message = self.str_node(node)
+        message = self.str_node(node, level)
         if (self.nodeType == "print"):
             print('  ' * level + message)
         for field, value in ast.iter_fields(node):
@@ -60,7 +126,7 @@ class CodeVisitor(ast.NodeVisitor):
         self.nodeType = "print"
         self.ast_visit(node)
 
-    def visit_FunctionDef(self, node):
+    def visit_MethodName(self, node):
         self.nodeType = "FunctionDef"
         self.ast_visit(node) 
         return self.methodName
@@ -83,18 +149,38 @@ def getAPISequence(code):
 def getMethodName(code):
     cv = CodeVisitor()
     tree = ast.parse(code)
-    return cv.visit_FunctionDef(tree)
+    return cv.visit_MethodName(tree)
     #cv.generic_visit(tree)
 
 def getToken(code):
     tree = ast.parse(code)
-    return sorted({node.id for node in ast.walk(tree) if isinstance(node, ast.Name)})
+    array = sorted({node.id for node in ast.walk(tree) if isinstance(node, ast.Name)})
+    result = []
+    for i in array:
+        result += camelSplit(i)
+    return result
+
+def camelSplit(word):
+    result = []
+    temp = word[0]
+    for i in word[1:]:
+        if (i.isupper()):
+            result.append(temp.lower())
+            temp = i
+        else:
+            temp += i
+    result.append(temp.lower())
+    return result
+
+
 
 
 code = "def add(arg1, arg2):\n\tprint(arg1)\n\treturn arg1+arg2\nadd(1,2)\n"
-code2 = "class A:\n\tdef a():\n\t\tprint(\"x\")\n\tdef b():\n\t\tself.a()\ntest=A()\ntest.b()\n"
+code2 = "class SomeClass:\n\tdef printFirst():\n\t\tprint(\"x\")\n\tdef printSecond():\n\t\tself.printFirst()\nsomeInstance=SomeClass()\nsomeInstance.printSecond()\n"
 #print(getMethodName(code))
 #print(getToken(code))
 #print(getAPISequence(code))
-printAST(code2)
+#printAST(code2)
 print(getAPISequence(code2))
+print(getMethodName(code2))
+print(getToken(code2))
